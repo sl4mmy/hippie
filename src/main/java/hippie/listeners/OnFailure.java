813a40737d8bc6cf70850bd62965e3ccd065eb78
Kent R. Spillner <kspillner@acm.org>
2009-10-03 13:46:34 -0500

    Adding notification strategies

---
+++ src/main/java/hippie/listeners/OnFailure.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright (c) 2009, Kent R. Spillner <kspillner@acm.org>
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+package hippie.listeners;
+
+import hippie.notifiers.NagiosNotifier;
+
+/**
+ * Understands how to notify Nagios that external service endpoints are
+ * not working correctly.
+ */
+public class OnFailure implements Strategy {
+        private final NagiosNotifier notifier;
+
+        public OnFailure(final NagiosNotifier notifier) {
+                this.notifier = notifier;
+        }
+
+        public void execute(final String name, final String message) {
+        }
+
+        public String getMessage() {
+                return null;
+        }
+
+        public Strategy failed() {
+                return null;
+        }
+
+        public Strategy ignored() {
+                return null;
+        }
+
+        public Strategy succeeded() {
+                return null;
+        }
+}
